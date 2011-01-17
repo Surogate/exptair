1ed163a7f41b553a744db274e1a9eae378b79faf
FrancoisAncel@gmail.com <FrancoisAncel@gmail.com@de5c70e0-22c0-d4e0-dbec-3d143463bd6f>
2011-01-17 22:26:50 +0000

    cleaning for future content

--- trunk/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include	<iostream>
-
-int		main(int argc, char *argv[])
-{
-  std::cout << "Salut Errata, allez hop hop hop on se met au boulot :)\n"
-	    << "Et pas de chichi, meme en entreprise !!!"
-	    << std::endl;
-  return 0;
-}
