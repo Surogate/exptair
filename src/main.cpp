7a3421f24cf57a01f3ba5ac25bea9a3b1da83665
extazmania <extazmania@de5c70e0-22c0-d4e0-dbec-3d143463bd6f>
2011-01-17 11:14:32 +0000

    First blood: directories

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include	<iostream>
+
+int		main(int argc, char *argv[])
+{
+  std::cout << "Salut Errata, allez hop hop hop on se met au boulot :)\n"
+	    << "Et pas de chichi, meme en entreprise !!!"
+	    << std::endl;
+  return 0;
+}
