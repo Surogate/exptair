fbb2ec8d0dfd9b6c7600c1085b56caae2fe80590
FrancoisAncel@gmail.com <FrancoisAncel@gmail.com@de5c70e0-22c0-d4e0-dbec-3d143463bd6f>
2011-01-20 07:32:29 +0000

    forgeting add file

---
+++ trunk/inc/Singleton.hpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   Singleton.hpp
+ * Author: Errata
+ *
+ * Created on 19 janvier 2011, 23:34
+ */
+
+#ifndef SINGLETON_HPP
+#define	SINGLETON_HPP
+
+template <typename T>
+class Singleton {
+public:
+
+    static T& Instance() {
+        static T instance;
+        return instance;
+    }
+
+protected:
+
+    Singleton() {
+    }
+
+    ~Singleton() {
+    }
+};
+
+#define SINGLETON_CLASS(CLASS) friend class Singleton<CLASS>;
+
+#endif	/* SINGLETON_HPP */
+
